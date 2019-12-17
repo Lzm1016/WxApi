9e115cedec9dd412ca5177ced9dc98485f172284
lzm <923425198@qq.com>
2019-12-17 10:57:23 +0800

    Initial commit

---
+++ Api/__init__.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+# -*- coding:utf-8 -*-
+
+# @Author : Liu
+# @FileName : __init__.py.py
+# @Time : 2019/12/17 10:51 AM
+
+
---
+++ app.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+# -*- coding:utf-8 -*-
+
+# @Author : Liu
+# @FileName : app.py
+# @Time : 2019/12/17 10:51 AM
+
+
+from flask import Flask
+
+
+def App():
+    app = Flask(__name__)
+    return app
